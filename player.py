2fc73fcf1cefe90cf5da2e2bd3708c30038b35ce
Daniel Čech <cechdaniel2008@gmail.com>
2025-07-16 18:35:27 +0200

    added stamina

--- player.py
+++ player.py
@@ -1,11 +1,14 @@
 class Player:
-    def __init__(self, name, health=100, max_health=100, money=0, inventory=None):
+    def __init__(self, name, equipped_weapon, health=100, max_health=100, money=0, stamina=100, max_stamina=100, inventory=None):
         # If no inventory was given, create a new empty list
         if inventory is None:
             inventory = []
 
         self.name = name
+        self.equipped_weapon = equipped_weapon
         self.health = health
         self.max_health = max_health
         self.money = money
+        self.stamina = stamina
+        self.max_stamina = max_stamina
         self.inventory = inventory
