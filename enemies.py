23d4c75323e415d13a2307a9b4f407a560529f4c
Daniel Čech <cechdaniel2008@gmail.com>
2025-07-16 22:13:53 +0200

    setup class

---
+++ enemies.py
@@ -0,0 +1,10 @@
+# ENEMY CLASS AND ENEMY LIST
+
+class Enemy:
+    def __init__(self,name,health,max_health,dmg,loot):
+        self.name = name
+        self.health = health
+        self.max_health = max_health
+        self.dmg = dmg
+        self.loot = loot
+
