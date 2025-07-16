0d954fbc5d0f64c254b00e21bafdcccb1b53d4cd
Daniel Čech <cechdaniel2008@gmail.com>
2025-07-16 18:35:13 +0200

    added stam drain

--- weapons.py
+++ weapons.py
@@ -6,12 +6,23 @@ class WeaponType(Enum):
     MAGIC = 'magic'
 
 class Weapon:
-    def __init__(self,name,type : WeaponType,info,dmg,value):
+    def __init__(self,name,type : WeaponType,info,dmg,stamina_drain,value):
         self.name = name
         self.type = type
         self.info = info
         self.dmg = dmg
+        self.stamina_drain = stamina_drain
         self.value = value
 
 #Name, Type, Info, Damage, Value
-wood_sword = Weapon('Wooden Sword',WeaponType.MELEE,'Weak wooden sword',10,30)
+
+# =======
+#  MELEE
+# =======
+
+wood_sword = Weapon('Wooden Sword',WeaponType.MELEE,'Weak wooden sword',10,6,30)
+#rusty_sword
+#sledge_hammer
+#thief_dagger
+#spear
+#steel_halberd
