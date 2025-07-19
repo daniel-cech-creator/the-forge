0054ee194ae1a357a2e0841bd751e10c82f36b0d
Daniel Čech <cechdaniel2008@gmail.com>
2025-07-19 20:26:46 +0200

    new weapons

--- weapons.py
+++ weapons.py
@@ -16,15 +16,15 @@ class Weapon:
         self.stamina_drain = stamina_drain
         self.value = value
 
-#Name, Type, Info, Damage, Value
+#Name, Type, Info, Damage, Stamina Drain, Value
 
 # =======
 #  MELEE
 # =======
 
 wood_sword = Weapon('Wooden Sword',WeaponType.MELEE,'Weak wooden sword',10,6,30)
-#rusty_sword
-#sledge_hammer
-#thief_dagger
+rusty_sword = Weapon('Rusty Sword',WeaponType.MELEE,'Old, damaged blade',14,8,28)
+sledge_hammer = Weapon('Sledge Hammer',WeaponType.MELEE,'Huge heavy blunt weapon',28,18,58)
+thief_dagger = Weapon('Thief Dagger', WeaponType.MELEE, 'Small dagger used by bandits',8,3,25)
 #spear
 #steel_halberd
