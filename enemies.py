3a0d47e2343001e6b0a0f9da27b3a0bbdb73e8af
Daniel Čech <cechdaniel2008@gmail.com>
2025-07-19 20:24:56 +0200

    attack func

--- enemies.py
+++ enemies.py
@@ -8,3 +8,17 @@ class Enemy:
         self.dmg = dmg
         self.loot = loot
 
+    def attack_turn(self,defending):
+        if defending is True:
+            print(f'{self.name} attacked you for {self.dmg/2} damage.')
+        else:
+            print(f'{self.name} attacked you for {self.dmg} damage.')
+
+
+# --- Entrance enemies ---
+
+#   Name, Health, Max Health, Damage, Loot
+dummy = Enemy('Training Dummy', 30, 30, 0, 0)
+giant_rat = Enemy('Giant Rat', 35, 35, 4, 0)
+
+enemy_pool = [dummy,giant_rat]
