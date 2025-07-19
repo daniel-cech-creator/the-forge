bbbdfc6e29d5e0edd2020ee9024f18c4cc774112
Daniel Čech <cechdaniel2008@gmail.com>
2025-07-19 20:26:01 +0200

    stat func

--- player.py
+++ player.py
@@ -1,10 +1,10 @@
 # PLAYER CLASS AND FUNCTIONS
 
 class Player:
-    def __init__(self, name, equipped_weapon, health=100, max_health=100, money=0, stamina=100, max_stamina=100, inventory=None):
-        # If no inventory was given, create a new empty list
+    def __init__(self, name, equipped_weapon, health=100, max_health=100, money=0, stamina=100, max_stamina=100, inventory=None, defending=False):
+        
         if inventory is None:
-            inventory = []
+            inventory = [None,None,None,None,None]
 
         self.name = name
         self.equipped_weapon = equipped_weapon
@@ -14,3 +14,8 @@ class Player:
         self.stamina = stamina
         self.max_stamina = max_stamina
         self.inventory = inventory
+        self.defending = defending
+
+    def stat_check(self):
+        if self.health > self.max_health: self.health= self.max_health
+        if self.stamina > self.max_stamina: self.stamina= self.max_stamina
