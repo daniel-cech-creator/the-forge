0fc5adac007a209cb0437897d94845946b40ca0e
Daniel Čech <cechdaniel2008@gmail.com>
2025-07-19 20:25:30 +0200

    battle loop

--- main.py
+++ main.py
@@ -5,14 +5,13 @@ import weapons
 from player import Player
 import inventory
 import enemies
-from utils import *
+import utils
 
 
-
-clear()
+utils.clear_term()
 player = Player(f'{str(input('Name your vessel: '))}',weapons.wood_sword)
 if player.name == '' :player.name = 'Vessel'
-clear()
+utils.clear_term()
 
 #Title
 print(r'''
@@ -24,7 +23,7 @@ print(r'''
                |___/      
 ''')
 input('Enter to continue')
-clear()
+utils.clear_term()
 
 #Game Loop
 while player.health > 0:
@@ -45,11 +44,89 @@ while player.health > 0:
     if choice == '1':
         print('\nGoing to the next room...')
         input()
+
+        #Choosing random room
+        match random.randint(1,3):
+
+            #Enemy room
+            case 1:
+                current_enemy = random.choice(enemies.enemy_pool)
+                current_enemy.health = current_enemy.max_health
+                
+                #Battle loop
+                while current_enemy.health > 0 and player.health > 0:
+                    utils.clear_term()
+                    print(f'{player.name} HP - {player.health}/{player.max_health}')
+                    print(f'{player.name} Stamina - {player.stamina}/{player.max_stamina}\n')
+                    print(f'{current_enemy.name} HP - {current_enemy.health}/{current_enemy.max_health}')
+                    print('-----------------')
+                    print('What will you do?\n')
+                    print('1 = Attack | 2 = Inventory | 3 = Defend')
+                    choice = str(input('> '))
+
+                    # Attacking
+                    if choice == '1':
+                        if player.stamina > player.equipped_weapon.stamina_drain:
+
+                            current_enemy.health -= player.equipped_weapon.dmg
+                            player.stamina -= player.equipped_weapon.stamina_drain
+                            print(utils.red(f'\nYou attacked for {player.equipped_weapon.dmg} damage.'))
+                            input()
+                        else:
+                            print(utils.blue('\nNot enough stamina!'))
+                            input()
+                            continue
+                    
+                    # Defending
+                    elif choice == '3':
+                        player.defending = True
+                        player.stamina += 15
+                        print('\nYou are defending!')
+                        input()
+
+                    # Invalid input
+                    else:
+                        print(utils.red('Invalid input!'))
+                        input()
+                        continue
+                    
+                    # ENEMY TURN
+                    if current_enemy.health > 0:
+                        current_enemy.attack_turn(player.defending)
+                        if player.defending is True:
+                            player.health -= current_enemy.dmg/2
+                        else:
+                            player.health -= current_enemy.dmg
+                        input()
+
+                        if player.defending == True:
+                            print(utils.yellow('You are no longer defending.'))
+                            player.defending = False
+                            input()
+                    else:
+                        print('The enemy died before it could attack.')
+                        input()
+                    player.stat_check()
+
+            #Chest room 
+            case 2:
+                print('chest :)')
+                input()
+            
+            #Empty room
+            case 3:
+                print('nothing :(')
+                input()
+        
+
     elif choice == '2':
         print('\nOpening inventory')
         input()
     else:
-        print(red('\nInvalid input!'))
+        print(utils.red('\nInvalid input!'))
         input()
     
-    clear()
+    utils.clear_term()
+
+
+#fix enemy health not reseting
