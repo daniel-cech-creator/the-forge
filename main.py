8f6a1854eb9bf451cf6fbe318aa08f207ed69daf
Daniel Čech <cechdaniel2008@gmail.com>
2025-08-16 23:55:55 +0200

    added special encounters

--- main.py
+++ main.py
@@ -26,13 +26,16 @@ if player.name == '' :player.name = 'Vessel'
 utils.clear_term()
 
 #Game Loop
+roomCount = 0
 while player.health > 0:
+    utils.clear_term()
     
     # Stats
     print(f'{utils.red('HP')} - {utils.red(player.health)}/{utils.red(player.max_health)}')
     print(f'{utils.blue('Stamina')} - {utils.blue(player.stamina)}/{utils.blue(player.max_stamina)}')
     print(f'{utils.yellow('Money')} - {utils.yellow(player.money)}')
     print(f'Equipped Weapon - {utils.green(player.equipped_weapon.name)} | {utils.red(f'{player.equipped_weapon.dmg} DMG')} | {utils.blue(f'{player.equipped_weapon.stamina_drain} Stamina Drain')}')
+    print(f'room count: {roomCount}')
     print('-----------------')
     print(utils.azure('What will you do?\n'))
     print('1 = Next room | 2 = Inventory')
@@ -45,6 +48,48 @@ while player.health > 0:
         print('\nGoing to the next room...')
         input()
 
+
+        if roomCount != 0 and roomCount % 10 == 0:
+            print(utils.yellow("A friendly voice welcomes you as you take step into the shopkeeper's keep."))
+            input()
+            roomCount += 1
+            continue
+
+        # Special room (5% chance)
+        if random.random() <= 0.5:
+            match random.randint(1,3):
+
+                # Magical Fountain
+                case 1:
+                    print('Your path was interrupted by a mystical fountain...')
+                    input()
+                    utils.clear_term()
+                    print(utils.azure('What will you do?\n'))
+                    print('\n1 = Drink the water | 2 = Leave')
+                    choice = str(input('> '))
+                    utils.clear_term()
+                    if choice == '1':
+                        print('You took a sip from the fountain...')
+                        input()
+                        print('Your health was replenished!')
+                        player.health = player.max_health
+                        
+                    else:
+                        print("You've moved on without drinking the water.")
+                        
+                
+                # Crystal Ball
+                case 2:
+                    print('crystal ball')
+
+                # Totem of Solitude
+                case 3:
+                    print('totem of solitude')
+            input()
+            roomCount += 1
+            continue
+        
+        
         #Choosing random room
         match random.randint(1,3):
 
@@ -121,11 +166,13 @@ while player.health > 0:
                     items.loot_display(battle_loot, current_enemy.loot, player)
                     player.inventory.extend(battle_loot)
                     input()
+                    roomCount += 1
                 else:
                     utils.clear_term()
                     print(utils.red('You lost!'))
                     input()
                     exit()
+            
 
             #Chest room 
             case 2:
@@ -137,6 +184,7 @@ while player.health > 0:
                 items.loot_display(chest_loot, random.randint(15,85),player)
                 player.inventory.extend(chest_loot)
                 input()
+                roomCount += 1
             
             #Empty room
             case 3:
@@ -146,6 +194,8 @@ while player.health > 0:
                     found_coins = random.randint(45,95)
                     player.money += found_coins
                     print(utils.blue(f'But you found {found_coins} coins!'))
+                    input()
+                roomCount += 1
 
     # OPEN INVENTORY
     elif choice == '2':
