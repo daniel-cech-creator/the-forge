9c631dfa44bd1924d30adae8fbc0431e5135b020
Daniel Čech <cechdaniel2008@gmail.com>
2025-09-03 22:43:45 +0200

    added new items

--- items.py
+++ items.py
@@ -3,6 +3,7 @@ from time import sleep
 import utils
 import random
 from collections import Counter
+from enum import Enum
 
 class Item:
     def __init__(self,name,info,rarity):
@@ -10,21 +11,40 @@ class Item:
         self.info = info
         self.rarity = rarity
 
-meaty_slop = Item('Meaty Slop', 'Heals for +40 Health', 'common')
-spaghetti_code = Item('spaghetti_code', 'player.health += 30', 'common')
-heart_charm = Item('Heart Charm', '+10 Maximum Health', 'rare')
-inside_friend = Item('Inside Friend', ' friend inside me.', 'epic')
-shadow_shell = Item('Shadow Shell', '+12 Defense', 'legendary')
+
+class Rarity(Enum):
+    COMMON = 'common'
+    RARE = 'rare'
+    EPIC = 'epic'
+    LEGENDARY = 'legendary'
+
+# COMMON
+meaty_slop = Item('Meaty Slop', 'Heals for +40 Health', Rarity.COMMON)
+spaghetti_code = Item('spaghetti_code', 'player.health += 30', Rarity.COMMON)
+
+# RARE
+heart_charm = Item('Heart Charm', '+10 Maximum Health', Rarity.RARE)
+dusek_sweets = Item("Dusek Sweets", "For your hard work", Rarity.RARE)
+
+# EPIC
+inside_friend = Item('Inside Friend', ' friend inside me.', Rarity.EPIC)
+
+# LEGENDARY
+shadow_shell = Item('Shadow Shell', '+12 Defense', Rarity.LEGENDARY)
+slopware = Item("Slopware", "I worked at Blizzard for 1000 years.", Rarity.LEGENDARY)
 
 
 def color_by_rarity(item):
-    if item.rarity == "common":
+    if item.rarity == Rarity.COMMON:
         return item.name
-    elif item.rarity == "rare":
+    
+    elif item.rarity == Rarity.RARE:
         return utils.blue(item.name)
-    elif item.rarity == "epic":
+    
+    elif item.rarity == Rarity.EPIC:
         return utils.purple(item.name)
-    elif item.rarity == "legendary":
+    
+    elif item.rarity == Rarity.LEGENDARY:
         return utils.rainbow(item.name)
 
 def loot_display(battle_loot,gold,player):
@@ -46,6 +66,9 @@ item_pool = {
     shadow_shell: 0.1, # 10%
     meaty_slop: 0.2, # 20%
     spaghetti_code: 0.25, # 25%
+    slopware: 0.05, # 5%
+    dusek_sweets: 0.12 # 12%
+
 }
 
 def roll_loot(item_pool):
